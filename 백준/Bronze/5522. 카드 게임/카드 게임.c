e8cd2dbf3618f455058f6dbbdcfaaac0554d6498
우주섢 <97648066+songyeon7@users.noreply.github.com>
2022-09-16 21:48:54 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a, b, c, d, e;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    scanf("%d", &d);
+    scanf("%d", &e);
+    printf("%d", a+b+c+d+e);
+    return 0;
+}
