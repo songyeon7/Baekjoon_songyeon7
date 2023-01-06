1b84c03c79220e467c6d6b9ebf0078f0d502a1c6
songyeon7 <97648066+songyeon7@users.noreply.github.com>
2023-01-06 15:23:05 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 1316 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,51 @@
+
+#include<cstdio>
+#include<queue>
+
+using namespace std;
+
+int map[105][105];
+int visit[105][105];
+
+int main()
+{
+  int N, M;
+  scanf("%d %d", &N, &M);
+  for(int i=1; i<=N; i++)
+    {
+      for(int j=1; j<=M; j++)
+        {
+          scanf("%1d", &map[i][j]);
+        }
+    }
+  queue<pair<int, int>> q;
+  q.push({1, 1});
+  visit[1][1]=1;
+  while(!q.empty())
+    {
+      int x=q.front().first, y=q.front().second;
+      q.pop();
+      if(map[x-1][y]&&!visit[x-1][y])
+      {
+        q.push({x-1, y});
+        visit[x-1][y] = visit[x][y]+1;
+      }
+      if(map[x+1][y]&&!visit[x+1][y])
+      {
+        q.push({x+1, y});
+        visit[x+1][y] = visit[x][y]+1;
+      }
+      if(map[x][y-1]&&!visit[x][y-1])
+      {
+        q.push({x, y-1});
+        visit[x][y-1] = visit[x][y]+1;
+      }
+      if(map[x][y+1]&&!visit[x][y+1])
+      {
+        q.push({x, y+1});
+        visit[x][y+1] = visit[x][y]+1;
+      }
+      
+    }
+  printf("%d", visit[N][M]);
+}
